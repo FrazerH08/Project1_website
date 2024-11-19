f817df553183c9bc74229d90a2ec75ed8f683d3d
Frazer Harness <654994@eastridingcollege.ac.uk>
2024-11-19 17:46:04 +0000

    Blah blah  blah

--- messaround.php
+++ messaround.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Testing</title>
-    <link rel="stylesheet" href="messaround.css">
+    <link rel="stylesheet" href=".css">
  </head>
  <body>
     <h1> I love this brilliant game 
