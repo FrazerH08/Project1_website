5f37b742e6113a6dd797b731d738d8ae6b401f82
Frazer Harness <654994@eastridingcollege.ac.uk>
2024-11-25 22:56:26 +0000

    All Changes blah blah

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="index.css"
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <div class="Create-post-icon">
+            <a href="new_post_form.php">
+            <img src="createpost.svg">
+            </a>
+        <div class="bookmark-icon">
+            <a href="bookmarked_posts.php">
+            <img src="Bookmark.svg">
+            </a>
+        <div class="delete-icon">
+            <a href="delete_profile.php">
+            <img src="Delete.svg">
+            </a>
+        <div class="view-profile">
+            <a href="view_profile.php">
+            <img src="Account.svg">
+            </a>
+        </div>
+        <div class="search-posts">
+            <a href="list_posts.php">
+            <img src="Search.svg">
+            </a>
+        </div>
+        <div class="login-icon">
+            <a href="login.php">
+            <img src="login.svg">
+            </a>
+        </div>
+        <div class="signup-icon">
+            <a href="signup.php">
+            <img src="signup.svg">
+            </a>
+        </div>
+</div>
+    <div class="Homepagetitle"> <h1> <u>Homepage</u></h1>
+</body>
+</html>
