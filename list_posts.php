3f5c24195acb648c2d836de87ca7f644699602d5
Frazer Harness <654994@eastridingcollege.ac.uk>
2024-11-29 14:25:50 +0000

    updates

--- list_posts.php
+++ list_posts.php
@@ -15,20 +15,20 @@ include 'connectdb.php';
 </head>
 <body>
 <nav class="Header">
+        <a class="icon" class="icon" href="index.php">
+            <img src="Home.svg">
+            </a>
             <a class="icon" href="new_post_form.php">
             <img src="create_post.svg">
             </a>
+            <a class="icon" href="list_posts.php">
+            <img src="Search.svg">
         <a class="icon" href="bookmarked_posts.php">
         <img src="Bookmark.svg">
-        </a>
-        <a class="icon" href="delete_profile.php">
-        <img src="delete_profile.svg">
         </a>
             <a class="icon" href="view_profile.php">
             <img src="Account.svg">
             </a>
-            <a class="icon" href="list_posts.php">
-            <img src="Search.svg">
             </a>
             <a class="icon" href="login.php">
             <img src="login.svg">
@@ -36,9 +36,10 @@ include 'connectdb.php';
             <a class="icon" href="signup.php">
             <img src="signup.svg">
             </a>
-            <a class="icon" class="icon" href="index.php">
-            <img src="Home.svg">
-            </a>
+            <a class="icon" href="delete_profile.php">
+        <img src="delete_profile.svg">
+        </a>
+
     </nav>
     <h1 class="title">All Posts</h1>
     <div class="search-button">
